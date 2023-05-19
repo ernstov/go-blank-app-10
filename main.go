63902c2002ca6467394bda410f60add95d47dcde
Ernst <35345611+ernstov@users.noreply.github.com>
2023-05-19 11:57:05 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	// Environment variables are defined in service.yaml file.
+	// Let's read the PORT, ADMIN_USERNAME and ADMIN_PASSWORD example variables:
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	adminUsername := os.Getenv("ADMIN_USERNAME")
+	adminPassword := os.Getenv("ADMIN_PASSWORD")
+
+	// Write a message to the console
+	fmt.Println("This is a golang example custom service.")
+	fmt.Printf("A web server will be started at port %s\n\n", port)
+	fmt.Println("Also, two example variables have been read:")
+	fmt.Printf("Admin username: %s\n", adminUsername)
+	fmt.Printf("Admin password: %s\n", adminPassword)
+	fmt.Println()
+	fmt.Println("Visit web interface to see this message there.")
+
+	// Create and register web server handler function
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintln(w, "This is a golang example custom service.")
+		fmt.Fprintf(w, "This web server is listening at port %s\n\n", port)
+		fmt.Fprintln(w, "Also, two example variables have been read:")
+		fmt.Fprintf(w, "Admin username: %s\n", adminUsername)
+		fmt.Fprintf(w, "Admin password: %s\n", adminPassword)
+
+	})
+
+	// Start web server
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+
+}
